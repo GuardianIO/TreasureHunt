23e8866a9c3c33fed6a3e5c2dd897277c8b093df
ccheung0926 <cindycheung0926@gmail.com>
2015-09-25 16:48:32 -0700

    [feat] added query IN PROGRESS

--- server/db/index.js
+++ server/db/index.js
@@ -8,4 +8,32 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-module.exports = connection;
+module.exports = {
+  createGameName: { 
+    function(params, cb) {
+      var queryStr = "INSERT into gameTable(game_name) VALUES(?)";
+      connection.query(queryStr, params, function(err, results){
+        if(err){
+          console.log(err);
+        }
+        else{
+          // 
+        }
+      });
+    }
+  },
+  createTreasureInfo: {
+    function(params, cb){
+    }
+  },
+  getPlayerContact: {
+    function(params,cb) {
+
+    }
+  }
+}
+
+// gameName
+// timelineKey, geolocation{lat:, long}, hint, image
+
+// module.exports = connection;
