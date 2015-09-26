bc7acfd1c4dd4654901f93a1c7936f2cdeeba3c1
codeCommode <james.harlan@gmail.com>
2015-09-25 20:16:05 -0700

    [bug] fixed deprecated invocation of body-parser

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ var requestHandlers = require('./requestHandlers');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser);
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
 
 // console.log(__dirname + /)
