ad53cc36f32741a29645233ee442cc06d5ab0bfe
codeCommode <james.harlan@gmail.com>
2015-10-01 16:03:30 -0700

    Revert "[feat] implemented geo checking in game controller"
    This reverts commit ee1fdb2210ee66787ff7aa5688a2b1acc6e7a7d0.

--- client/app/controllers/game.js
+++ client/app/controllers/game.js
@@ -1,14 +1,18 @@
 angular.module('treasureHunt.game', ['treasureHunt.services'])
-.controller('GameCtrl', ['$scope', '$location', '$interval', 'RequestFactory',
-  function($scope, $location, $interval, RequestFactory){
+.controller('GameCtrl', ['$scope', '$location', 'RequestFactory',
+  function($scope, $location, RequestFactory){
     $scope.clue = '';
     var node = 0;
+<<<<<<< HEAD
 
     $scope.arrived = false;
     $scope.currentNode = {};
 
+=======
+>>>>>>> Revert "[feat] implemented geo checking in game controller"
     updateNode = function(nodeNum){
-      $scope.currentNode =  RequestFactory.getNode(nodeNum);
+      angular.extend($scope.currentNode,RequestFactory.getNode(nodeNum));
+      console.log($scope.currentNode);
     }
 
     getGame = function(){
@@ -17,55 +21,48 @@ angular.module('treasureHunt.game', ['treasureHunt.services'])
         RequestFactory.getGame(gameId);
       }
     };
-
-    checkCoords = function(data){
-      var coords = data.coords;
-      var x = coords.latitude - $scope.currentNode.lat;
-      var y = coords.longitude - $scope.currentNode.lon;
-      var distance = Math.sqrt( (x)*(x) + (y)*(y) );
-      console.log($scope.currentNode.lat);
-      if(distance < 0.0001){
-        $scope.arrived = true;
-        console.log('arrived!');
-      }else{
-        console.log(distance);
-      }
-
-    };
-
-    $interval(function(){
-      updateNode(node);
-      navigator.geolocation.getCurrentPosition(checkCoords);
-    }, 1000);
-
-    checkCoords = function(data){
-      var coords = data.coords;
-      var x = coords.latitude - $scope.currentNode.lat;
-      var y = coords.longitude - $scope.currentNode.lon;
-      var distance = Math.sqrt( (x)*(x) + (y)*(y) );
-      console.log($scope.currentNode.lat);
-      if(distance < 0.0001){
-        $scope.arrived = true;
-        console.log('arrived!');
-      }else{
-        console.log(distance);
-      }
-
-
-    };
-
-    $interval(function(){
-      updateNode(node);
-      navigator.geolocation.getCurrentPosition(checkCoords);
-    }, 1000);
-    // navigator.geolocation.watchPosition(function(loc){
-    //   updateNode(node);
-    //   $scope.$apply($scope.currentLocation = loc.coords);
-    //   checkCoords();
-    // });
+    updateNode = function(){
     
+    };
+    checkCoords = function(){
+
+    };
+    navigator.geolocation.watchPosition(function(loc){
+      $scope.$apply(angular.extend($scope.currentLocation, loc.coords))
+    }
+    
+    $interval(function(){
+      updateNode(node);
+      navigator.geolocation.getCurrentPosition(checkCoords);
+    }, 1000);
+
+    checkCoords = function(data){
+      var coords = data.coords;
+      var x = coords.latitude - $scope.currentNode.lat;
+      var y = coords.longitude - $scope.currentNode.lon;
+      var distance = Math.sqrt( (x)*(x) + (y)*(y) );
+      console.log($scope.currentNode.lat);
+      if(distance < 0.0001){
+        $scope.arrived = true;
+        console.log('arrived!');
+      }else{
+        console.log(distance);
+      }
+
+
+    };
+
+    $interval(function(){
+      updateNode(node);
+      navigator.geolocation.getCurrentPosition(checkCoords);
+    }, 1000);
+
     getGame();
-    navigator.geolocation.getCurrentPosition(checkCoords);
+
+    $scope.currentNode;
+    if(!$scope.currentNode){
+      updateNode(node);
+    }
     $scope.next = function(){
       updateNode(++node);
     }
