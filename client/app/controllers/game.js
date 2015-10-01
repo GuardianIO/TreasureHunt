05015394583a165223ba6a6429aa21d5bd471224
codeCommode <james.harlan@gmail.com>
2015-10-01 15:19:18 -0700

    [feat] implemented geo checking in game controller

--- client/app/controllers/game.js
+++ client/app/controllers/game.js
@@ -3,12 +3,16 @@ angular.module('treasureHunt.game', ['treasureHunt.services'])
   function($scope, $location, $interval, RequestFactory){
     $scope.clue = '';
     var node = 0;
+<<<<<<< HEAD
 
     $scope.arrived = false;
+=======
+    $scope.arrived = false;
+    $scope.currentNode = {};
+>>>>>>> [feat] implemented geo checking in game controller
 
     updateNode = function(nodeNum){
-      angular.extend($scope.currentNode,RequestFactory.getNode(nodeNum));
-      console.log($scope.currentNode);
+      $scope.currentNode =  RequestFactory.getNode(nodeNum);
     }
 
     getGame = function(){
@@ -17,6 +21,7 @@ angular.module('treasureHunt.game', ['treasureHunt.services'])
         RequestFactory.getGame(gameId);
       }
     };
+<<<<<<< HEAD
 
     checkCoords = function(data){
       var coords = data.coords;
@@ -37,14 +42,41 @@ angular.module('treasureHunt.game', ['treasureHunt.services'])
       updateNode(node);
       navigator.geolocation.getCurrentPosition(checkCoords);
     }, 1000);
+=======
 
-    getGame();
+    checkCoords = function(data){
+      var coords = data.coords;
+      var x = coords.latitude - $scope.currentNode.lat;
+      var y = coords.longitude - $scope.currentNode.lon;
+      var distance = Math.sqrt( (x)*(x) + (y)*(y) );
+      console.log($scope.currentNode.lat);
+      if(distance < 0.0001){
+        $scope.arrived = true;
+        console.log('arrived!');
+      }else{
+        console.log(distance);
+      }
+>>>>>>> [feat] implemented geo checking in game controller
 
-    $scope.currentNode;
-    if(!$scope.currentNode){
+    };
+
+    $interval(function(){
       updateNode(node);
+<<<<<<< HEAD
     }
     
+=======
+      navigator.geolocation.getCurrentPosition(checkCoords);
+    }, 1000);
+    // navigator.geolocation.watchPosition(function(loc){
+    //   updateNode(node);
+    //   $scope.$apply($scope.currentLocation = loc.coords);
+    //   checkCoords();
+    // });
+    
+    getGame();
+    navigator.geolocation.getCurrentPosition(checkCoords);
+>>>>>>> [feat] implemented geo checking in game controller
     $scope.next = function(){
       updateNode(++node);
     }
