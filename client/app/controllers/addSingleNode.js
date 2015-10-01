c86ed69e613354479b7452e6e784bf9ba5286c22
tbutman <tbutman@gmail.com>
2015-09-30 20:16:51 -0700

    [feat] integrate db with front-end

---
+++ client/app/controllers/addSingleNode.js
@@ -0,0 +1,25 @@
+angular.module('treasureHunt.singleNode', ['treasureHunt.services'])
+
+.controller('AddSingleNodeCtrl', ['$scope', '$location', 'SendPicAndLoc', 
+  function($scope, $location, SendPicAndLoc){
+
+    SendPicAndLoc.getLoc();
+    
+    $scope.status = {
+      canUpload:false
+    };
+
+    $scope.$on('locReady', function(){
+      $scope.$apply($scope.status.canUpload = true);
+    });
+
+    $scope.send = function(){
+      if($scope.file){
+        SendPicAndLoc.sendPic($scope.file);
+      }
+    };
+
+    $scope.createGame = function(){
+      $location.path('/invite');
+    }
+}]);
