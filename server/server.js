6ffee320fbefef1d021c3f573a660d7820e0e9ac
ccheung0926 <cindycheung0926@gmail.com>
2015-10-02 17:32:18 -0700

    [feat] added the new route for finishing page

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ app.get('/download/:url', requestHandlers.downloadHandler);
 app.post('/games', requestHandlers.getAllGames);
 app.post('/addWaypoint', requestHandlers.uploadHandler);
 
+app.post('/finishGame', requestHandlers.getLastGameInfo);
 app.post('/gameInfo', requestHandlers.getGameInfo);
 app.post('/game', requestHandlers.getGame);
 app.post('/createGame', requestHandlers.createGame);
