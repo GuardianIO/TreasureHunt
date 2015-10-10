822b60424cf9f0c2a7c1a22f6123840e4f54a6f3
codeCommode <james.harlan@gmail.com>
2015-10-09 20:05:14 -0700

    [feat] setting location from map

--- client/app/controllers/map.js
+++ client/app/controllers/map.js
@@ -1,11 +1,12 @@
-angular.module('treasureHunt.map', ['ngMap', 'ui.router'])
-.controller('MapCtrl', ['$scope', '$location','$state', function($scope, $location, $state){
+angular.module('treasureHunt.map', ['ngMap', 'ui.router', 'treasureHunt.services'])
+.controller('MapCtrl', ['$scope', '$location','$state', '$interval', 'SendPicAndLoc',
+  function($scope, $location, $state, $interval, SendPicAndLoc){
 
   $scope.initializeMap = function(){
     if(navigator&&navigator.geolocation){
       navigator.geolocation.getCurrentPosition(function(pos){
         var coords = pos.coords;
-        map = new google.maps.Map(document.getElementById('map'), {
+        var map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: coords.latitude, lng: coords.longitude},
             scrollwheel: true,
             zoom: 15
@@ -13,8 +14,8 @@ angular.module('treasureHunt.map', ['ngMap', 'ui.router'])
         var myMarker;
         function placeMarker(location) {
           if(location){
-            $scope.$parent.userCoords.latitude = location.J;
-            $scope.$parent.userCoords.longitude = location.M;
+            $scope.userCoords.latitude = location.J;
+            $scope.userCoords.longitude = location.M;
           }
           if ( myMarker ) {
             myMarker.setPosition(location);
@@ -25,7 +26,8 @@ angular.module('treasureHunt.map', ['ngMap', 'ui.router'])
               draggable: true
             });
           }
-        };//placeMarker()
+        };
+
         placeMarker({lat: coords.latitude, lng: coords.longitude});
 
         myMarker.setPosition(map.getCenter());
@@ -34,17 +36,6 @@ angular.module('treasureHunt.map', ['ngMap', 'ui.router'])
               console.log(event.latLng);
               placeMarker(event.latLng);
             }); 
-        // google.maps.event.addListener(myMarker, 'dragend', function (evt) {
-        //       $scope.geoCoords.latitude = evt.latLng.lat().toFixed(3);
-        //       $scope.geoCoords.latitude = evt.latLng.long().toFixed(3);
-        //         // document.getElementById('current').innerHTML = '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(3) + ' Current Lng: ' + evt.latLng.lng().toFixed(3) + '</p>';
-        //     });
-        // google.maps.event.addListener(myMarker, 'dragstart', function (evt) {
-        //       console.log("working")
-        //       $scope.geoCoords.latitude = evt.latLng.lat();
-        //       $scope.geoCoords.latitude = evt.latLng.long();
-        //         // document.getElementById('current').innerHTML = '<p>Currently dragging marker...</p>';
-        //     });
         map.setCenter(myMarker.position);
         myMarker.setMap(map);
 
@@ -52,16 +43,38 @@ angular.module('treasureHunt.map', ['ngMap', 'ui.router'])
 
         $scope.watchPosition = navigator.geolocation.watchPosition(function(loc){
           if($scope.map && !$scope.$parent.mapShow){
-            $scope.map.setCenter(new google.maps.LatLng(loc.coords.latitude, loc.coords.longitude));
+            console.log('map center');
+            $scope.map.setCenter(new google.maps.LatLng(loc.coords.latitude, loc.coords.longitude));            
           }
         });
-      });//getCurrentPosition
-      
-    }//if
-  };//initializeMap()
+      }); 
+    }
+  };
+
+  var interval = $interval(function(){
+    var map = $('#map')
+    if(map){
+      google.maps.event.trigger(document.getElementById('map'), 'resize');
+    }else{
+      $interval.cancel(interval);
+    }
+  },300);
+
+  $scope.$on('$destroy', function(){
+    $interval.cancel(interval);
+    navigator.geolocation.cancelWatch($scope.watchPosition);
+  })
 
   $scope.setCoordinates = function(){
     $scope.$parent.mapShow = !$scope.$parent.mapShow;
+    SendPicAndLoc.loc = {
+      coords:{
+        latitude:$scope.userCoords.latitude,
+        longitude:$scope.userCoords.longitude,
+        accuracy:1,
+        timestamp:Date.now()
+      }
+    };
   }
 
 }]);
