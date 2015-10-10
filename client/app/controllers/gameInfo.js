eacb7e17595d7364c2a99e1971c802267cccd887
ccheung0926 <cindycheung0926@gmail.com>
2015-10-09 19:05:09 -0700

    [feat] created new files and route for About page

--- client/app/controllers/gameInfo.js
+++ client/app/controllers/gameInfo.js
@@ -3,11 +3,6 @@ angular.module('treasureHunt.gameInfo',['treasureHunt.services'])
 .controller('GameInfoCtrl', ['$scope','RequestFactory', '$location', '$state', '$stateParams',
  function($scope, RequestFactory, $location, $state, $stateParams){
   var vm = this;
-  // $scope.nuts = {
-  //   grayNut: "../../../img/nut-gray.png",
-  //   fullNut: "../../../img/nut-full.png",
-  //   halfNut: "../../../img/nut-half.png"
-  // };
 
   vm.gameId = $location.url().split('/').pop();
   $scope.startGame = function(){
