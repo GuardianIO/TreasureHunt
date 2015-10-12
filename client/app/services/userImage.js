11c5d17ec7b59256279daab3b1169c6487146131
codeCommode <james.harlan@gmail.com>
2015-10-12 16:19:53 -0700

    [feat] service to upload/retrieve user node images

---
+++ client/app/services/userImage.js
@@ -0,0 +1,29 @@
+angular.module('userImage', ['ngFileUpload'])
+
+.factory('SendUserImage', ['Upload', '$http', function(Upload, $http){
+  return {
+    send:function(file, cb){
+      Upload.upload(file)
+      .success(function(data, status, headers, config){
+        cb(data);
+      })
+      .error(function(){
+        console.log('error status: ' status);
+      })
+    },
+    getNodeImages:function(gameData, cb){
+      $http.post('/userImages',
+        gameData
+      })
+      .then(
+        function(res){
+          //success callback
+          cb(res.data);
+        }, 
+        function(res){
+          //error callback
+          console.error('error:'res.data);
+      })
+    }
+  }
+}]);
